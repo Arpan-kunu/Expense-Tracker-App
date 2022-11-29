5b846ca1b860ae57ac7867c62ee966c17f55f116
Arpan <baral.arpan1997@gmail.com>
2022-11-29 09:01:48 +0530

    Intial commit

---
+++ All Files/frontend/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Container from "./components/Container";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Expense-App</h2>
+      <Container />
+    </div>
+  );
+}
+
+export default App;
